bd0a68a6bb75df7c45e43b2f79774fbbac7a2460
vClaudio11 <vonclaudio.college@gmail.com>
2026-06-20 11:39:47 +0800

    chore: add pg pool connection with dotenv config

---
+++ db.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const { pool } = require('pg')
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+})
+
+module.exports = pool
