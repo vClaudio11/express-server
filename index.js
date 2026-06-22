e03463e3cc3a92b6f7a7db0ca659aa6d03f23643
vClaudio11 <vonclaudio.college@gmail.com>
2026-06-22 11:30:13 +0800

    feat: add PUT and DELETE routes with full error handling

--- index.js
+++ index.js
@@ -15,15 +15,49 @@ app.get('/about', (req, res) => {
 const habits = []
 
 app.post('/habits', async (req, res) => {
-    const { name, frequency } = req.body
-    if (!name || !frequency) {
-        return res.status(400).json({ error: 'Name and frequency are required'})
+    try {
+        const { name, frequency } = req.body
+        if (!name || !frequency) {
+            return res.status(400).json({ error: 'Name and frequency are required'})
+        }
+        const result = await pool.query(
+            'INSERT INTO habits (name, frequency) VALUES ($1, $2) RETURNING *',
+            [name, frequency]
+        )
+        res.status(201).json(result.rows[0])
+    } catch (err) {
+        return res.status(500).json({ error: 'Failed to create habit'})
     }
-    const result = await pool.query(
-        'INSERT INTO habits (name, frequency) VALUES ($1, $2) RETURNING *',
-        [name, frequency]
-    )
-    res.status(201).json(result.rows[0])
+})
+
+app.delete('/habits/:id', async (req, res) => {
+    try{
+        const { id } = req.params
+        const result = await pool.query(
+            'DELETE FROM habits WHERE id = $1',
+            [id]
+        )
+        return res.status(200).json({ message: 'Habit deleted'})
+    } catch (err) {
+        return res.status(500).json({ error: 'Failed to delete habit'})
+    }
+})
+
+app.put('/habits/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const { name, frequency } = req.body
+        if (!name || !frequency) {
+            return res.status(400).json({ error: 'Name and frequency are required'})
+        }
+        const result = await pool.query(
+            'UPDATE habits SET name = $1, frequency = $2 WHERE id = $3 RETURNING *',
+            [name, frequency, id]
+        )
+        res.status(200).json(result.rows[0])
+    } catch(err) {
+        res.status(500).json({ error: 'Failed to update habit'})
+    }
 })
 
 app.get('/habits', async (req, res) => {
