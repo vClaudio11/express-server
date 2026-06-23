71fda99bf9f058498112fa59a136d02b67a638a9
vClaudio11 <vonclaudio.college@gmail.com>
2026-06-23 15:23:46 +0800

    feat: add successful login auth between frontend and backend

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
 const pool = require('./db')
 const authRouter = require('./routes/auth')
 const verifyToken = require('./middleware/auth')
 const habitsRouter = require('./routes/habits')
-const cors = require('cors')
 
+app.use(cors({ origin: 'http://localhost:5173' }))
 app.use(express.json())
 app.use('/auth', authRouter)
 app.use('/habits', verifyToken, habitsRouter)
-app.use(cors({ origin: 'http://localhost:5173' }))
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
