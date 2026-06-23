ddf1aaf95c9bde9eb954cb6c87bd52b95d065e26
vClaudio11 <vonclaudio.college@gmail.com>
2026-06-23 11:30:46 +0800

    chore: add CORS middleware for React frontend origin

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const pool = require('./db')
 const authRouter = require('./routes/auth')
 const verifyToken = require('./middleware/auth')
 const habitsRouter = require('./routes/habits')
+const cors = require('cors')
 
 app.use(express.json())
 app.use('/auth', authRouter)
 app.use('/habits', verifyToken, habitsRouter)
+app.use(cors({ origin: 'http://localhost:5173' }))
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
