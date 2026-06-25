3f7cf5fb61ceecc1cb3eb40b24693b1ff5636822
vClaudio11 <vonclaudio.college@gmail.com>
2026-06-25 09:25:00 +0800

    chore: add Railway dynamic port

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.use('/auth', authRouter)
 app.use('/weekly-log', verifyToken, weeklyLogRouter)
 app.use('/habits', verifyToken, habitsRouter)
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Listening on port ${process.env.PORT} || 3000`)
 })
